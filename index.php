52de7140298bede6dbf47e455fd1e776211e7108
shivanipatil01 <77020815+shivanipatil01@users.noreply.github.com>
2021-02-21 17:56:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="shortcut icon" href="images/cm.png">
+   
+	 <link rel="stylesheet" href="index_button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("images/aa.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+
+	<div id="header">
+       <br>
+       <h1 style=" font-family:Agency FB; font-size: 70px; color:#ec4646;text-shadow: 2px 2px black;"> The Sparks Foundation Bank </h1>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#663f3f;text-shadow: 2px 2px black;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Online Transactions</h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="primary_btn" id="button "type="button" href="getdetail.php">View users</button>
+                </a>
+
+               
+                </tr>
+
+                <tr>        
+               <br> <br> <br>
+			   <a href="transfer.php">
+			   <button class="primary_btn" id="button" type="button">Credit Transfer</button>
+               </a>
+               
+               </tr>
+			   
+			    <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="primary_btn" id="button" type="button">All Transaction</button>
+               </a>
+
+            </table>
+    </div>
+	<div class="footer" style="font-size:32px; font-family: Agency; color:#fff; margin-top:10px;">
+	<p>Made for the GRIP internship program</p>
+	<p>Made by Shivani Patil</p>
+	</div>
+	           
+</body>
+</html>
